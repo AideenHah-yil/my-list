2bbdeddb6ba20beecedab886ac04f0416460a56c
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-07 01:57:55 -0800

    updates

--- list.py
+++ list.py
@@ -8,7 +8,27 @@ print("After Append:", my_list)
 
 my_list.insert[1] = 15
 
+# another list
 my_list = [50, 60, 70]
 
+#adding two lists using extend
 my_list.extend(my_list)
+
+print("After Extend:", my_list)
+
+#remove last element
+
+my_list.pop[-1]
+
+print("After Pop:", my_list)
+
+#sort list in ascending order
+
+my_list.sort()
+print("After Sort:", my_list)
+
+# find index of 30
+
+my_list.index(30)
+
 
